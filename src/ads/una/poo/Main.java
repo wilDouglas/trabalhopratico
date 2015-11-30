9ebf7605003388b3fbfce09a7586959135bc7199
William Silva <williamsilva808@hotmail.com>
2015-11-30 20:44:34 -0200

    alterando a classe main

--- src/ads/una/poo/Main.java
+++ src/ads/una/poo/Main.java
@@ -5,15 +5,44 @@
  */
 package ads.una.poo;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 
 
 /**
  *
- * @author william
+ * @author William Silva
+ * @author Cintia Ferreira
  */
 public class Main {
 
     public static void main(String[] args) {
 
+        Produto p = new ProdutoPerecivel(30,123456, "Açucar", "Açucar Tia Juana", "Alimentos", 5);
+        
+        ArrayList<Produto> produtos = new ArrayList<>();
+        produtos.add(new ProdutoPerecivel(30,123456, "Açucar", "Açucar Tia Juana", "Alimentos", 5));
+        produtos.add(new ProdutoPerecivel(30,665655, "Açucar", "Açucar Tia Juana", "Alimentos", 5));
+        produtos.add(new ProdutoPerecivel(30,252114, "Açucar", "Açucar Tia Juana", "Alimentos", 5));
+        
+        Calendar vencimento = Calendar.getInstance();
+        vencimento.set(Calendar.DAY_OF_MONTH, 30);
+        vencimento.set(Calendar.MONTH, 11);
+        vencimento.set(Calendar.YEAR, 2016);
+        
+        Lote lote  = new Lote(produtos,2.25,Calendar.getInstance(),vencimento);
+        
+        Estoque estoque = new Estoque();
+        estoque.registrarLote(lote);
+        
+        System.out.println(estoque.obterQuantidadeDisponivel(p));
+        
+        Venda venda = new Venda();
+        ItemProduto item = new ItemProduto();
+        item.setProdutos(produtos);
+        venda.adicionarItemProduto(item);
+        
+        System.out.println(venda.obterPrecoTotal());
     }
 }
